6a4743ae4d60ae30ea8f97fc1fe384cd4edbd9c0
Andre Luis Miranda <almiranda86@gmail.com>
2020-01-24 08:14:09 -0300

    Promises

--- JavaScriptFundamentals/src/index.js
+++ JavaScriptFundamentals/src/index.js
@@ -1,5 +1,14 @@
 import { HelloWorld } from './helloWorld';
+import { ApiClient } from './apiClient';
 
 new HelloWorld().sayHello();
 
+var apiClient = new ApiClient();
+apiClient.getTasks()
+.then(data => {
+    console.log(`I got back ${data.length} rows`);
+})
+.catch(err => {
+    console.log(`Something went horribly wrong: ${err}`);
+})
 
